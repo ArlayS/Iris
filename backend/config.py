0c6420bb614ff5f39ebf702e5690d7da4cac5ba3
Aurélie SGARD <aurelie.sgard@icloud.com>
2026-07-28 02:13:32 +0200

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -21,6 +21,7 @@ DISCORD_HELPER_ROLE_ID = os.environ.get("DISCORD_HELPER_ROLE_ID")
 DISCORD_ADMIN_ROLE_ID = os.environ.get("DISCORD_ADMIN_ROLE_ID")
 DISCORD_TICKET_CATEGORY_ID = os.environ.get("DISCORD_TICKET_CATEGORY_ID")
 EMERGENT_LLM_KEY = os.environ.get("EMERGENT_LLM_KEY")
+DISCORD_STAFF_ROLE_ID = os.environ.get("DISCORD_STAFF_ROLE_ID")
 
 
 def missing_discord_bot_settings() -> list[str]:
