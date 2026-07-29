fd3a4974e799bce7efce9ee67bf72f853d98de56
Aurélie SGARD <aurelie.sgard@icloud.com>
2026-07-29 16:25:28 +0200

    Update animateur.py

--- backend/routers/animateur.py
+++ backend/routers/animateur.py
@@ -1,4 +1,4 @@
-from config import DISCORD_HELPER_ROLE_ID, DISCORD_HELPER_ROLE2_ID
+
 from services.discord_service import DiscordService
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
@@ -6,12 +6,11 @@ from database import get_db
 import models, schemas
 
 router = APIRouter(prefix="/animateur/projects", tags=["animateur-projects"])
-
-@router.get("/", response_model=list[schemas.ProjectOut])
+@router.get("", response_model=list[schemas.ProjectOut])
 def list_projects(db: Session = Depends(get_db)):
     return db.query(models.Project).order_by(models.Project.id.desc()).all()
 
-@router.post("/", response_model=schemas.ProjectOut)
+@router.post("", response_model=schemas.ProjectOut)
 def create_project(project: schemas.ProjectCreate, db: Session = Depends(get_db)):
     db_project = models.Project(**project.dict())
     db.add(db_project)
